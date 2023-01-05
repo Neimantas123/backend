b34da45bdddb78d6325b40822167803e7aedc50b
Neimantas <samochvalovas.neimantas@gmail.com>
2023-01-05 12:49:23 +0200

    mongo db sign in

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -17,30 +20,19 @@ mongoose
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, nextt) => {
+  res.status(500).send({ message: err.message });
 });
 
-app.get('/api/products/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
-
-app.get('/api/product/:_id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params._id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () => {
-  console.log('Server is running');
+  console.log('Server is running' + port);
 });
